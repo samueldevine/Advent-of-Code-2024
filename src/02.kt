201d18799827bf456d986ae9b663a551d106c4c2
Sam Devine <sam.devine@ibotta.com>
2024-12-02 17:24:02 -0700

    day 2 problem 2

--- src/02.kt
+++ src/02.kt
@@ -4,29 +4,46 @@ fun main() {
     val input = File("data/02.txt")
     var safe = 0
 
-    var lineNumber = 0
     input.forEachLine { report ->
-        lineNumber++
-        var reportSafe = true
         val strLevels: List<String> = report.split(" ")
         val levels = strLevels.map { it.toInt() }
-        val increasing: Boolean = levels[0] <= levels[1]
 
-        for (i in levels.indices) {
-            if (i == 0) continue // skip first level
-
-            val diff = levels[i] - levels[i - 1]
-            if (increasing && (diff < 1 || diff > 3)) {
-                reportSafe = false
-                break
+        if (isReportSafe(levels) == -1) {
+            safe ++
+        } else {
+            var isSafeAfterRemoval = false
+            for (i in levels.indices) {
+                val filteredLevels = levels.toMutableList().apply { removeAt(i) }
+                if (isReportSafe(filteredLevels) == -1) {
+                    isSafeAfterRemoval = true
+                    break
+                }
             }
-
-            if (!increasing && (diff < -3 || diff > -1)) {
-                reportSafe = false
-                break
+            if (isSafeAfterRemoval) {
+                safe++
             }
         }
-        if (reportSafe) { safe += 1 }
     }
     println(safe)
+}
+
+fun isReportSafe(levels: List<Int>): Int {
+    // return index where report was deemed unsafe, or -1 if report is safe
+
+    val increasing: Boolean = levels[0] <= levels[1]
+
+    for (i in levels.indices) {
+        if (i == 0) continue // skip first level
+
+        val diff = levels[i] - levels[i - 1]
+        if (increasing && (diff < 1 || diff > 3)) {
+            return i
+        }
+
+        if (!increasing && (diff < -3 || diff > -1)) {
+            return i
+        }
+    }
+
+    return -1
 }
